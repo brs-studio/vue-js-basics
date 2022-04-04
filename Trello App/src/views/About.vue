cda21f6ff9ba9fcd45215b42aba9239f09f8c003
Pavana sri vinaya <52523178+pavanasri-nyros@users.noreply.github.com>
2022-04-04 15:26:10 +0000

    Add files via upload

---
+++ Trello App/src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="board">
+   <div class="about">
+    <router-link to="/">Home</router-link>
+    <br>
+    <router-link to="/about">About</router-link>
+    </div>
+    <br>
+    <h1> About the app </h1>
+    <p>
+      This is the trello board: We can add the task related details.It has
+      columns to rearrange the tasks between Inprogress, todo, done.
+    </p>
+  </div>
+</template>
+<style scoped>
+.about {
+  margin-top: 30px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 12px;
+}
+</style>
